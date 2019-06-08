d8cf46ac9c6dc5a6d62f2b0c6dd2c7586134758d
Tomomasa UCHIDA <tomomasa.is.0930@gmail.com>
2019-06-09 01:00:26 +0900

    update

--- py/predict_image.py
+++ py/predict_image.py
@@ -1,10 +1,15 @@
-# USAGE
-#  $ export FLASK_APP=predict_image.py
-#  $ flask run
-
 import os
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
+
+from keras.models import Sequential, load_model
+import keras,sys
+import numpy as np
+from PIL import Image
+
+classes = ["building","person","tree"]
+num_of_classes = len(classes)
+image_size = 50
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
@@ -21,27 +26,50 @@ def upload_file():
         if 'file' not in request.files:
             flash('ファイルがありません')
             return redirect(request.url)
+
         file = request.files['file']
         if file.filename == '':
             flash('ファイルがありません')
             return redirect(request.url)
+            
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file', filename=filename))
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            model = load_model('./cnn_model_aug.h5')
+
+            image = Image.open(filepath)
+            image = image.convert('RGB')
+            image = image.resize((image_size, image_size))
+            data = np.asarray(image)
+            X = []
+            X.append(data)
+            X = np.array(X)
+
+            result = model.predict([X])[0]
+            predicted = result.argmax()
+            percentage = int(result[predicted] * 100)
+
+            return "ラベル： " + classes[predicted] + ", 確率："+ str(percentage) + " %"
+
+
+            #return redirect(url_for('uploaded_file', filename=filename))
     return '''
     <!doctype html>
     <html>
-    <head>
-    <meta charset="UTF-8">
-    <title>ファイルをアップロードして判定しよう</title></head>
-    <body>
-    <h1>ファイルをアップロードして判定しよう！</h1>
-    <form method = post enctype = multipart/form-data>
-    <p><input type=file name=file>
-    <input type=submit value=Upload>
-    </form>
-    </body>
+        <head>
+            <meta charset="UTF-8">
+            <title>ファイルをアップロードして判定しよう</title>
+        </head>
+        
+        <body>
+            <h1>ファイルをアップロードして判定しよう！</h1>
+            <form method = post enctype = multipart/form-data>
+                <p><input type=file name=file>
+                <input type=submit value=Upload>
+            </form>
+        </body>
     </html>
     '''
 
